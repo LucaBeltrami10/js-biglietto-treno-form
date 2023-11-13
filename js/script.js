87b8fa36dd25ac946afb931cacd4360f77b62628
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-13 16:05:38 +0100

    corretto parsefloat su distance

--- js/script.js
+++ js/script.js
@@ -8,8 +8,8 @@ const inputDiscount = document.querySelector('main div button')
 inputDiscount.addEventListener('click', function(){
 
     /* inserite variabili dentro ricaricare sempre il loro valore all'input sul button rilevato dal listener */
-    let userAge = parseFloat(document.getElementById('user-Age').value)
-    let userDistance = parseInt(document.getElementById('user-distance').value)
+    let userAge = parseInt(document.getElementById('user-Age').value)
+    let userDistance = parseFloat(document.getElementById('user-distance').value)
     /* mostrato il valore in console */
 
     console.log(`User age: ${userAge}`)
