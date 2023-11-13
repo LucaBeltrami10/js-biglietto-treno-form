382a73b9b6f511a849411705b71a054b1066eafc
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-13 16:04:37 +0100

    inseriti calcoli e condizioni all'interno listener

--- js/script.js
+++ js/script.js
@@ -3,27 +3,33 @@
 /* definisco l'elemento da "ascoltare" */
 const inputDiscount = document.querySelector('main div button')
 
+
 /* definisco cosa rilevare e quali output produrre) */
 inputDiscount.addEventListener('click', function(){
+
     /* inserite variabili dentro ricaricare sempre il loro valore all'input sul button rilevato dal listener */
     let userAge = parseFloat(document.getElementById('user-Age').value)
     let userDistance = parseInt(document.getElementById('user-distance').value)
     /* mostrato il valore in console */
+
     console.log(`User age: ${userAge}`)
     console.log(`User distance: ${userDistance}`)
 
+    /* definisco il prezzo del viaggio */
+    let ticketCost = userDistance * 0.1976;
+
+    /* mostro in console prezzo intero */
+    console.log(`prezzo intero: ${ticketCost}`)
+
+    /* applico condizioni per lo sconto */
+    if(userAge >= 65){
+        ticketCost *= 0.4673
+    } else if(userAge < 18){
+        ticketCost *= 0.8235
+    }
+    /* mostro in console il prezzo scontato */
+    console.log(`prezzo finale: ${ticketCost}`)
+    console.log(`prezzo finale arrotondato: ${ticketCost.toFixed(2)}`)
+
 })
-
-
-/* definisco condizioni e formula per calcolo dello sconto */
-
-let fullPrice = userDistance * 0.1976;
-let discountPrice;
-
-if(userAge >= 65){
-    discountPrice = userDistance * 0.4673
-} else if(userAge < 18){
-    discountPrice = userDistance * 0.8235
-}
-
 
