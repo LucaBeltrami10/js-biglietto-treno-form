13cf1d75219d9a27ae18be504ee6d74d8783c34f
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-13 15:35:43 +0100

    inserita condizione per discount

--- js/script.js
+++ js/script.js
@@ -6,8 +6,8 @@ const inputDiscount = document.querySelector('main div button')
 /* definisco cosa rilevare e quali output produrre) */
 inputDiscount.addEventListener('click', function(){
     /* inserite variabili dentro ricaricare sempre il loro valore all'input sul button rilevato dal listener */
-    let userAge = document.getElementById('user-Age').value
-    let userDistance = document.getElementById('user-distance').value
+    let userAge = parseFloat(document.getElementById('user-Age').value)
+    let userDistance = parseInt(document.getElementById('user-distance').value)
     /* mostrato il valore in console */
     console.log(`User age: ${userAge}`)
     console.log(`User distance: ${userDistance}`)
@@ -15,4 +15,15 @@ inputDiscount.addEventListener('click', function(){
 })
 
 
-/*  */
+/* definisco condizioni e formula per calcolo dello sconto */
+
+let fullPrice = userDistance * 0.1976;
+let discountPrice;
+
+if(userAge >= 65){
+    discountPrice = userDistance * 0.4673
+} else if(userAge < 18){
+    discountPrice = userDistance * 0.8235
+}
+
+
