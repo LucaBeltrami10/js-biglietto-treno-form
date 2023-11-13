b00f800c01f8d8ad922fc62aa02ce04f19e9144c
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-13 15:21:38 +0100

    ins. variabili userAge e distance dentro listener

--- js/script.js
+++ js/script.js
@@ -1,5 +1,3 @@
-let userAge = document.getElementById('user-Age').value
-let userDistance = document.getElementById('user-distance').value
 
 /* quando l'utente clicca il bottone recupero i dati in console */
 /* definisco l'elemento da "ascoltare" */
@@ -7,6 +5,9 @@ const inputDiscount = document.querySelector('main div button')
 
 /* definisco cosa rilevare e quali output produrre) */
 inputDiscount.addEventListener('click', function(){
+    let userAge = document.getElementById('user-Age').value
+    let userDistance = document.getElementById('user-distance').value
+
     console.log(userAge)
     console.log(userDistance)
 })
