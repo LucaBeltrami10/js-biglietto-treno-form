c921f1efcd8ac242cca50b92b4ba0f22be635fc7
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2023-11-13 15:25:55 +0100

    inserito commenti e modificato output in console

--- js/script.js
+++ js/script.js
@@ -5,12 +5,14 @@ const inputDiscount = document.querySelector('main div button')
 
 /* definisco cosa rilevare e quali output produrre) */
 inputDiscount.addEventListener('click', function(){
+    /* inserite variabili dentro ricaricare sempre il loro valore all'input sul button rilevato dal listener */
     let userAge = document.getElementById('user-Age').value
     let userDistance = document.getElementById('user-distance').value
+    /* mostrato il valore in console */
+    console.log(`User age: ${userAge}`)
+    console.log(`User distance: ${userDistance}`)
 
-    console.log(userAge)
-    console.log(userDistance)
 })
 
 
-
+/*  */
